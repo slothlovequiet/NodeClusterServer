50a9f2373a8c0493f9c16a8fb403748bd490acf5
slothlovequiet <79837937+slothlovequiet@users.noreply.github.com>
2021-10-31 14:46:09 +0500

    1

---
+++ worker.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const pid = process.pid;
+
+http
+    .createServer((req, res) => {
+      for (let i = 0; i < 1e7; i++) {}
+      res.end (`Hello from Node.js \n`)
+    }).listen(8800, () => {
+      console.log(`Server started. Pid: ${pid}`);
+    });
